0e23675129bfea3a4290f3450cb18db6a75f3cd7
Marcio Aurelio Sant'Ana <100080330+djmarciopira@users.noreply.github.com>
2025-09-30 19:51:13 -0300

    Add files via upload

---
+++ tarefas/ex4/professor.js
@@ -0,0 +1,12 @@
+class Professor {
+    constructor(nome, email, nascimento, fixo, celular, area, matricula, lattes) {
+        this.nome = nome;
+        this.email = email;
+        this.nascimento = nascimento;
+        this.fixo = fixo;
+        this.celular = celular;
+        this.area = area;
+        this.matricula = matricula;
+        this.lattes = lattes;
+    }
+}
